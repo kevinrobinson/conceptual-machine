d79ecce6a3ec61d73969fdeaa9ab2c46136e5c0f
kevinrobinson <kevin.robinson.0@gmail.com>
2019-11-12 12:45:31 -0500

    Try webpack?

---
+++ rollup.config.js
@@ -0,0 +1,9 @@
+// import resolve from 'rollup-plugin-node-resolve';
+
+module.exports = {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'iife'
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,59 @@
+import * as tf from '@tensorflow/tfjs';
+import * as tmImage from '@teachablemachine/image';
+import _ from 'lodash';
+
+// can't get it's module and Rollbar to play nicely together
+const { JSZip } = window;
+
+const state = {
+  projectZip: null
+};
+
+
+// function refresh() {
+//   console.log('refresh', state);
+// }
+
+function debug(...params) {
+  console.log(...params); // eslint-disable-line no-console
+}
+
+async function main() {
+  document.querySelector('#upload-file-input').addEventListener('change', async e => {
+    if (e.target.files.length === 0) return;
+    const [file] = e.target.files;
+    // state.projectZip = file;
+    // refresh();
+
+    const unzippedFiles = [];
+    const jsZip = new JSZip();
+    debug('Reading zip..');
+    jsZip.loadAsync(file).then((zip) => {
+      debug('reading...');
+      zip.forEach((relativePath, zipEntry) => {
+        debug('zipEntry', zipEntry);
+        unzippedFiles.push(zipEntry);
+      });
+    });
+
+    function findZip(filename) {
+      return _.find(unzippedFiles, (zip) => zip.name === filename);
+    }
+
+    async function readJson(zip) {
+      return JSON.parse(await zip.async('text'));
+    }
+
+    async function readBinary(zip) {
+      return JSON.parse(await zip.async('blob'));
+    }
+    const modelJson = await readJson(findZip('model.json'));
+    const metaDataJson = await readJson(findZip('metadata.json'));
+    const weightsBin = await readBinary(findZip('weights.bin'));
+
+    const model = await tmImage.loadFromFiles(modelJson, metaDataJson, weightsBin);
+    console.log('model', model);
+  });
+}
+
+main();
